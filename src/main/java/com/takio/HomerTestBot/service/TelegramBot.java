d0f6c565bbf3293d9a0ed6aca579f2ce59a07f00
IgorTeleychuk <teleychuk.i@yandex.r>
2023-05-19 16:09:27 +0700

    part_three_menu: create test menu

--- src/main/java/com/takio/HomerTestBot/service/TelegramBot.java
+++ src/main/java/com/takio/HomerTestBot/service/TelegramBot.java
@@ -4,17 +4,39 @@ import com.takio.HomerTestBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @Slf4j
 public class TelegramBot extends TelegramLongPollingBot  { // наследуем из библиотеки телеграм
     final BotConfig config;
+    static final String HELP_TEXT = "This bot is created to demonstrate Spring capabilities.\n\n" +
+            "You can execute commands from the main menu on the left or by typing a command:\n\n" +
+            "Type /start to see a welcome message\n\n" +
+            "Type /mydata to see data stored about yourself\n\n" +
+            "Type /help to see this message again";
 
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>(); // создаем меню для бота Класс BotCommand от библиотеки
+        listOfCommands.add(new BotCommand("/start", "get a welcome massage"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null)); // добавляем созданное меню в систему
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
 
     @Override
@@ -37,6 +59,9 @@ public class TelegramBot extends TelegramLongPollingBot  { // наследуем
                 case "/start":
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName()); // передаем Id чата и имя пользователя
                     break; // не забываем об этом
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break; // не забываем естественно
 
                 default:
                     sendMessage(chatId, "Sorry, command was not recognized");
